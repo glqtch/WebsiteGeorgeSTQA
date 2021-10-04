463fabb318b4e3a76496f2d2d04cf11b2282edeb
Semvl <330834@student.mboutrecht.nl>
2021-10-04 13:15:35 +0200

    Added: Landing page and Book event page. Changed: Some file names to fit current website.

--- Website/home.php
+++ Website/home.php
@@ -1,24 +1,29 @@
-<div class="row mt-3">
-    <span class="line"></span>
-    <div class="col-6 col-md-3 col-lg-3 col-xl-4">
+<div class="row">
+    <!-- <div class=col-container> -->
+    <div class="col-4 ">
         <div class="card mx-auto border-0">
             <div class="card-body text-center">
-                <p>Hele lap text over wat voor een restaurant dit is</p>
+                <p>George Marina is set in a spectacular, three storey waterside building in the upcoming Amstelkwartier, right along the banks of the Amstel river that flows into Amsterdam's iconic canals.
+‍                   With large windows overlooking the marina in front, an open kitchen and sushi counter, lush plants, a striking 360 green marble bar and an eye-catching staircase right in the middle of the restaurant.</p>
             </div>
         </div>
+        <hr/>
     </div>
-    <div class="col-6 col-md-3 col-lg-3 col-xl-4 ">
+    <div class="col-4">
         <div class="card mx-auto border-0">
             <div class="card-body text-center">
                 <img src="./img/home/george.png" alt="George">
             </div>
         </div>
+        <hr/>
     </div>
-    <div class="col-12 col-xl-4">
+    <div class="col-4">
         <div class="card mx-auto border-0">
             <div class="card-body text-center">
-                <p>Hele lap text over wat voor soort eten er te bieden is</p>
+                <p>Being so close to the water, we're famous for our seafood and sushi platters, but you might as well order steak frites, a juicy bistro burger or a veggie dish. A great all-day hangout for breakfast, lunch, dinner or Friday night cocktails accompanied by a live set from our resident DJ.</p>
             </div>
         </div>
+        <hr/>
     </div>
+    <!-- </div> -->
 </div>

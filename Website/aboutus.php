d72470b413b3eb037db83b5b99cc555eee3b768a
Quint Theissen <quinttheissen@gmail.com>
2021-10-04 13:16:31 +0200

    Added an aboutus page

---
+++ Website/aboutus.php
@@ -0,0 +1,66 @@
+<div class="row p-5">
+    <div class="col-12 col-md-12 col-lg-6 col-xl-6">
+        <div class="row">   
+            <div class="col-12">     
+                <h2>Hoe is george marina begonnen?</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Aliquam at augue volutpat, 
+                    placerat quam vitae, accumsan ipsum. Fusce feugiat ultricies ultrices. 
+                    Proin ultricies tortor quis tincidunt venenatis. Donec sagittis risus ac sodales porta. 
+                    Duis nec ante malesuada, euismod urna vel, imperdiet nunc. Integer neque purus, egestas vel diam nec, 
+                    lobortis suscipit orci. Nunc semper condimentum viverra. Integer placerat risus vitae vestibulum tincidunt. 
+                    Vivamus bibendum quis mauris eget sagittis.
+                    Etiam ullamcorper nibh a ultrices luctus. 
+                    Vestibulum ante ipsum primis in faucibus orci 
+                    luctus et ultrices posuere cubilia curae; Nam sit amet odio libero. 
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+                    Ut vel tortor facilisis, luctus nunc egestas, accumsan dui. In luctus, massa sed consectetur aliquet, 
+                    mi felis sagittis ligula, in venenatis diam augue in ipsum. Vivamus feugiat sit amet augue sit amet pharetra. 
+                    Nulla mollis rutrum elit sit amet aliquam.
+                </p>
+                <hr class="my-4"style="width:100%", color=black>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <img src="./img/photos/seafood_george.jpeg" alt="George" class="img-fluid w-100">
+                <hr class="my-4"style="width:100%", color=black>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-md-12 col-lg-6 col-xl-6 ">
+        <div class="row">
+            <div class="col-12">
+                <img src="./img/photos/restaurant-docks.png" alt="George" class="img-fluid w-100">
+                <hr class="my-4"style="width:100%", color=black>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-12">
+                <h2>Onze missie</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Aliquam at augue volutpat, 
+                    placerat quam vitae, accumsan ipsum. Fusce feugiat ultricies ultrices. 
+                    Proin ultricies tortor quis tincidunt venenatis. Donec sagittis risus ac sodales porta. 
+                    Duis nec ante malesuada, euismod urna vel, imperdiet nunc. Integer neque purus, egestas vel diam nec, 
+                    lobortis suscipit orci. Nunc semper condimentum viverra. Integer placerat risus vitae vestibulum tincidunt. 
+                    Vivamus bibendum quis mauris eget sagittis.
+                    Etiam ullamcorper nibh a ultrices luctus. 
+                    Vestibulum ante ipsum primis in faucibus orci 
+                    luctus et ultrices posuere cubilia curae; Nam sit amet odio libero. 
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+                    Ut vel tortor facilisis, luctus nunc egestas, accumsan dui. In luctus, massa sed consectetur aliquet, 
+                    mi felis sagittis ligula, in venenatis diam augue in ipsum. Vivamus feugiat sit amet augue sit amet pharetra. 
+                    Nulla mollis rutrum elit sit amet aliquam.
+                </p>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+    
+</div>

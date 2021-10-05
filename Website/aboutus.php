2a52abdd16a034fddade0cbe83abeb17df3c3d85
Quint Theissen <quinttheissen@gmail.com>
2021-10-05 12:12:46 +0200

    Refinement: replaced lorem ipsum with actual informative text

--- Website/aboutus.php
+++ Website/aboutus.php
@@ -2,38 +2,35 @@
     <div class="col-12 col-md-12 col-lg-6 col-xl-6">
         <div class="row">   
             <div class="col-12">     
-                <h2>Hoe is george marina begonnen?</h2>
+                <h2>Hoe is George Marina/MBO Utrecht begonnen?</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Aliquam at augue volutpat, 
-                    placerat quam vitae, accumsan ipsum. Fusce feugiat ultricies ultrices. 
-                    Proin ultricies tortor quis tincidunt venenatis. Donec sagittis risus ac sodales porta. 
-                    Duis nec ante malesuada, euismod urna vel, imperdiet nunc. Integer neque purus, egestas vel diam nec, 
-                    lobortis suscipit orci. Nunc semper condimentum viverra. Integer placerat risus vitae vestibulum tincidunt. 
-                    Vivamus bibendum quis mauris eget sagittis.
-                    Etiam ullamcorper nibh a ultrices luctus. 
-                    Vestibulum ante ipsum primis in faucibus orci 
-                    luctus et ultrices posuere cubilia curae; Nam sit amet odio libero. 
-                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
-                    Ut vel tortor facilisis, luctus nunc egestas, accumsan dui. In luctus, massa sed consectetur aliquet, 
-                    mi felis sagittis ligula, in venenatis diam augue in ipsum. Vivamus feugiat sit amet augue sit amet pharetra. 
-                    Nulla mollis rutrum elit sit amet aliquam.
+                   George marina is begonnen met het idee om op een mooie locatie zeevruchten te serveren. George Marina
+                   is dan ook niet het eerste restaurant in de George familie. In samenwerking met MBO Utrecht zetten wij zogenaamde "oefenrestaurants" op,
+                   waar leerlingen van MBO Utrecht de mogelijkheid krijgen om mee te werken aan het runnen van een heus restaurant. 
                 </p>
-                <hr class="my-4"style="width:100%", color=black>
+                <hr class="my-4 line"style="width:100%", color=black>
             </div>
-        </div>
-        <div class="row">
             <div class="col-12">
-                <img src="./img/photos/seafood_george.jpeg" alt="George" class="img-fluid w-100">
-                <hr class="my-4"style="width:100%", color=black>
+                <img src="./img/photos/employees.jpg" alt="Employees" class="img-fluid w-100">
+                <hr class="my-4 line"style="width:100%", color=black>
+            </div>
+            <div class="col-12">     
+                <h2>Onze recente prestaties</h2>
+                <p>
+                    Wij zijn trots op het feit dat wij bij George Marina zo een breed leerprogramma op hebben gezet samen met MBO Utrecht.
+                    Daltonlaan 300 is namelijk niet de enige locatie waarbij wij dit faciliteren, dit doen wij op zo veel mogelijk locaties
+                    om zo veel mogelijk leerlingen de mogelijkheid te geven. 
+                </p>
+                <hr class="my-4 line"style="width:100%", color=black>
             </div>
         </div>
+        
     </div>
     <div class="col-12 col-md-12 col-lg-6 col-xl-6 ">
         <div class="row">
             <div class="col-12">
-                <img src="./img/photos/restaurant-docks.png" alt="George" class="img-fluid w-100">
-                <hr class="my-4"style="width:100%", color=black>
+                <img src="./img/photos/restaurant-docks.png" alt="Docks_photo" class="img-fluid w-100">
+                <hr class="my-4 line"style="width:100%", color=black>
             </div>
         </div>
         
@@ -41,20 +38,11 @@
             <div class="col-12">
                 <h2>Onze missie</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Aliquam at augue volutpat, 
-                    placerat quam vitae, accumsan ipsum. Fusce feugiat ultricies ultrices. 
-                    Proin ultricies tortor quis tincidunt venenatis. Donec sagittis risus ac sodales porta. 
-                    Duis nec ante malesuada, euismod urna vel, imperdiet nunc. Integer neque purus, egestas vel diam nec, 
-                    lobortis suscipit orci. Nunc semper condimentum viverra. Integer placerat risus vitae vestibulum tincidunt. 
-                    Vivamus bibendum quis mauris eget sagittis.
-                    Etiam ullamcorper nibh a ultrices luctus. 
-                    Vestibulum ante ipsum primis in faucibus orci 
-                    luctus et ultrices posuere cubilia curae; Nam sit amet odio libero. 
-                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
-                    Ut vel tortor facilisis, luctus nunc egestas, accumsan dui. In luctus, massa sed consectetur aliquet, 
-                    mi felis sagittis ligula, in venenatis diam augue in ipsum. Vivamus feugiat sit amet augue sit amet pharetra. 
-                    Nulla mollis rutrum elit sit amet aliquam.
+                    De missie van deze samenwerking met MBO Utrecht is om studenten 
+                    de mogelijkheid te geven om te ervaren hoe het is om een restaurant te runnen.
+                    Onder begeleiding van hun mentor en persoonlijke leermeester zullen geinteresseerde studenten
+                    ontzettend veel ervaring vergaren in het restaurantleven.
+
                 </p>
                 
             </div>
